5ef3657dbf87ee3723c03d37daa5e70f86148836
safeya <safeyakhan25@gmail.com>
2026-07-08 18:26:13 +0400

    Add API health check route

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ const app = express();
 ========================= */
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+  res.send('Ecommerce API is running 🚀');
+});
 /* =========================
    ROUTES
 ========================= */
